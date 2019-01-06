de6fb6cdc43abf42a99e4c8c5f3f2b6752acdcf2
Unay Santisteban <usantisteban@othercode.es>
2019-01-06 20:52:18 +0100

    Some updates

--- setup.py
+++ setup.py
@@ -1,16 +1,24 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+import setuptools
 
-setup(
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
     name='FControllerPY',
     version='0.0.1',
     description='Container and Front Controller for modules or controllers.',
     author='Unay Santisteban',
     author_email='usantisteban@othercode.es',
     maintainer='Unay Santisteban',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url='https://github.com/othercodes/fcontroller-py',
-    packages=['fcontroller'],
-    license='MIT',
-    download_url='',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
